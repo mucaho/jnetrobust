2e28eafd0304d002dbe503b5c036e2a36f69754d
mucaho <mkucko@gmail.com>
2014-09-17 03:11:48 +0200

    Update license headers.

--- jnetrobust-samples/src/main/java/net/ddns/mucaho/jnetemu/DatagramWanEmulator.java
+++ jnetrobust-samples/src/main/java/net/ddns/mucaho/jnetemu/DatagramWanEmulator.java
@@ -1,6 +1,6 @@
 /**
  * The MIT License (MIT)
- * Copyright (c) 2013 github.com/mucaho
+ * Copyright (c) 2013 mucaho (http://mucaho.ddns.net/)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
